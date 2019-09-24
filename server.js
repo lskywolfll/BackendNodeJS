c51f78f5b283047c43b5eb016b37180b04f69cb4
René <renealejandrosanchezmorales@gmail.com>
2019-09-23 23:42:55 -0300

    add:Express,Router and Nodemon

--- server.js
+++ server.js
@@ -1,10 +1,33 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+const router = express.Router();
 
 var app = express();
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(router);
 
-app.use('/', (req, res) => {
-    res.send('Hola');
+router.get('/get', (req,res) => {
+    console.log(req.headers);
+    res.header({
+        "custom-header":"Nuestro valor personalizado"
+    });
+    res.send('Hola desde get');
 });
+
+router.post('/post', (req,res) => {
+    res.send('Hola desde Post');
+});
+
+router.delete('/delete', (req,res) => {
+    console.log(req.query);
+    console.log(req.body);
+    res.send(`Mensaje ${req.body.text} añadido correctamente`);
+});
+
+// app.use('/', (req, res) => {
+//     res.send('Hola');
+// });
 
 app.listen(3000);
 console.log('La aplicacion esta escuchando en http://localhost:3000');
